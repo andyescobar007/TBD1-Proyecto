bc51dfcfa694e11602d6fd0585c668210e38d46a
Andy Escobar <andyescobar007@unitec.edu>
2018-06-20 07:33:46 -0600

    Inicio

---
+++ src/src/Menu.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author ANDY ESCOBAR
+ */
+public class Menu {
+    public static void main(String[] args) {
+     JF_Login login=new JF_Login();
+     login.setVisible(true);
+    }
+    
+    
+}
