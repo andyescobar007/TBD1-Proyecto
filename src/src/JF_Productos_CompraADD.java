f8e80d63029c6f5ee489e6c58f12fc0e22757348
Andy Escobar <andyescobar007@unitec.edu>
2018-07-01 16:19:15 -0600

    bases de datos productos compra

--- src/src/JF_Productos_CompraADD.java
+++ src/src/JF_Productos_CompraADD.java
@@ -7,8 +7,6 @@ package src;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
 
 /**
